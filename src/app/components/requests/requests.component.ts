9583a6014b542937d9d72a8d3d3c8abba33459bb
Hossam-k911 <53270950+Hossam-k911@users.noreply.github.com>
2020-03-09 13:53:48 +0200

    editing request component

--- src/app/components/requests/requests.component.ts
+++ src/app/components/requests/requests.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AdminAuthService } from 'src/app/services/admin-auth.service';
 
 @Component({
@@ -8,16 +8,45 @@ import { AdminAuthService } from 'src/app/services/admin-auth.service';
 })
 export class RequestsComponent implements OnInit {
   requests: any
+  // time: String = ''
+  // userId: any
   constructor(public myAdminAuthService: AdminAuthService) { }
 
+  @Input() reqDetails: IreqDetails = {
+    name: '',
+    phone: '',
+    time: '',
+    date: '',
+    test: ''
 
+  }
   ngOnInit() {
 
     this.myAdminAuthService.getRequests().subscribe((response: any) => {
       this.requests = response;
+
     })
+
 
 
   }
 
+  handleProfileNavigation() {
+    this.myAdminAuthService.selectedRequest = this.reqDetails
+    // this.MyRouter.navigate(['/countries/profile', this.countryDetails],{skipLocationChange:true})
+    // this.MyRouter.navigate(['/countries/profile'])
+  }
+
+}
+
+
+
+
+
+interface IreqDetails {
+  name: string;
+  phone: string;
+  time: string;
+  date: string;
+  test: string;
 }
