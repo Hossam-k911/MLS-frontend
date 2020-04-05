008ddee5d98fdfa847d9137d0c40938d777d6bb1
Ahmed Mhamed AL Behairy <50558698+Ahmeedmohamed@users.noreply.github.com>
2020-04-05 10:47:45 +0200

    updata design content bage

--- src/app/components/requests/requests.component.ts
+++ src/app/components/requests/requests.component.ts
@@ -1,13 +1,14 @@
 //imports
-import { Component, OnInit } from '@angular/core';
-import { AdminAuthService } from 'src/app/services/admin-auth.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Component, OnInit } from "@angular/core";
+import { AdminAuthService } from "src/app/services/admin-auth.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 //configurations
+declare var $: any;
 @Component({
-  selector: 'app-requests',
-  templateUrl: './requests.component.html',
-  styleUrls: ['./requests.component.css']
+  selector: "app-requests",
+  templateUrl: "./requests.component.html",
+  styleUrls: ["./requests.component.css"]
 })
 export class RequestsComponent implements OnInit {
   editForm: FormGroup;
@@ -16,110 +17,94 @@ export class RequestsComponent implements OnInit {
     public myAdminAuthService: AdminAuthService,
     public modelService: NgbModal,
     public fb: FormBuilder
-  ) { }
-
+  ) {}
 
   // Initialization
-  requests: any
-  selectedRequest: any = {}
-  patientData: any = {}
-  req_status: any = ''
-  accepted_status: string = 'Accepted';
-  rejected_status: string = 'Rejected';
-  pending_status: string = 'Pending';
-  req_id: any = ''
-  p_id: any = ''
-
-
+  requests: any;
+  selectedRequest: any = {};
+  patientData: any = {};
+  req_status: any = "";
+  accepted_status: string = "Accepted";
+  rejected_status: string = "Rejected";
+  pending_status: string = "Pending";
+  req_id: any = "";
+  p_id: any = "";
 
   // Getting Requests and fill in table
   ngOnInit() {
     this.myAdminAuthService.getRequests().subscribe((response: any) => {
       this.requests = response;
-
-    })
-
+    });
   }
 
   getcurrentRowData(row) {
     this.selectedRequest = row;
     this.req_id = row._id;
     this.p_id = row.req_p_id;
-    this.getPatientData(row._id)
+    this.getPatientData(row._id);
   }
 
   // Adding Patient data
   getPatientData(row) {
-    const { req_id } = this
-    const reqData = { req_id }
+    const { req_id } = this;
+    const reqData = { req_id };
 
-    this.myAdminAuthService.reqPatientData(reqData).subscribe((response: any) => {
-      this.patientData = response;
-    })
-    console.log(this.patientData)
+    this.myAdminAuthService
+      .reqPatientData(reqData)
+      .subscribe((response: any) => {
+        this.patientData = response;
+      });
+    console.log(this.patientData);
   }
-
 
   //(accept-penting-reject ) status
   AcceptedStatus() {
-    const { req_id, req_status } = this
+    const { req_id, req_status } = this;
     const data = {
-      req_id, req_status
-    }
+      req_id,
+      req_status
+    };
     this.myAdminAuthService.statusChange(data).subscribe((response: any) => {
       this.req_status = this.accepted_status;
       this.ngOnInit();
       // console.log(this.req_status);
-
-
-    })
-
-
+    });
   }
   PendingStatus() {
-    const { req_id, req_status } = this
+    const { req_id, req_status } = this;
     const data = {
-      req_id, req_status
-    }
+      req_id,
+      req_status
+    };
     this.myAdminAuthService.statusChange(data).subscribe((response: any) => {
       this.req_status = this.pending_status;
       this.ngOnInit();
       // console.log(this.req_status);
       // this.ngOnInit();
-    })
-
-
+    });
   }
   RejectedStatus() {
-    const { req_id, req_status } = this
+    const { req_id, req_status } = this;
     const data = {
-      req_id, req_status
-    }
+      req_id,
+      req_status
+    };
     this.myAdminAuthService.statusChange(data).subscribe((response: any) => {
       this.req_status = this.rejected_status;
       this.ngOnInit();
       // console.log(this.req_status);
       // this.ngOnInit();
-
-
-    })
-
+    });
   }
 
   // Request Deletion
   deleteRequest() {
-    const { req_id, p_id } = this
-    const reqData = { req_id, p_id }
-    this.myAdminAuthService.deleteRequest(reqData).subscribe((response: any) => {
-      this.ngOnInit();
-    })
+    const { req_id, p_id } = this;
+    const reqData = { req_id, p_id };
+    this.myAdminAuthService
+      .deleteRequest(reqData)
+      .subscribe((response: any) => {
+        this.ngOnInit();
+      });
   }
-
-
-
-
-
-
 }
-
-
